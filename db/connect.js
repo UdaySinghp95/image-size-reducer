e4d032fc88c6e10dcdbe53e0ec65385004068abc
Uday Singh <79022546+UdaySinghp95@users.noreply.github.com>
2024-07-09 02:23:21 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,9 @@
 
 const mongoose = require('mongoose');
 
+console.log('--------------------Mongo URL ----------------',process.env.MONGO_URI)
+
+
 function connectDB(){
     mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
       .then(() => console.log('MongoDB connected'))
