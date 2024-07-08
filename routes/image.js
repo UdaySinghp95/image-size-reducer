6828accacd18d0a03530f5c235c0b663a7baf387
Uday Singh <79022546+UdaySinghp95@users.noreply.github.com>
2024-07-09 02:04:08 +0530

    Update image.js

--- routes/image.js
+++ routes/image.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 const multer = require('multer');
 const { processImageJob } = require('../processor/image');
-const uploadFile = multer({ dest: "uploads/" });
+const uploadFile = multer({ dest: "/temp/uploads/" });
 
 imageRoute.post('/add-image', async (req, res) => {
     const { productName, imageUrls } = req.body;
