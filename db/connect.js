1984d6904196e5ad971c7b4240addc031f35e94e
Uday Singh <79022546+UdaySinghp95@users.noreply.github.com>
2024-07-09 02:20:22 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,8 @@
-const MONGO_URI = 'mongodb://127.0.0.1:27017/image-process';
+
 const mongoose = require('mongoose');
 
-
 function connectDB(){
-    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
       .then(() => console.log('MongoDB connected'))
       .catch(err => console.log(err));
 }
